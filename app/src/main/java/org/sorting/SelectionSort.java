d5b5f511fa3eb147ddd2df1b5c01b8bf44110bfc
Alfred <arista21dominguez@gmail.com>
2025-06-03 10:49:20 -0600

    Generalizacion

--- app/src/main/java/org/sorting/SelectionSort.java
+++ app/src/main/java/org/sorting/SelectionSort.java
@@ -103,4 +103,29 @@ public class SelectionSort
             System.out.println("Ordenando ....... : " + Arrays.toString(arr));
         }
     }
+
+    public static <T extends Comparable<T>> void sort(T[] arr) 
+    {
+        for (int i = 0; i < arr.length - 1; i++) 
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) 
+            {
+                if (arr[j].compareTo(arr[minIndex]) < 0) 
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) 
+            {
+                T temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            System.out.println("Ordenando ....... : " + Arrays.toString(arr));
+        }
+    }
 }
